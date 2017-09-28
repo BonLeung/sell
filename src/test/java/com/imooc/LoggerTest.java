2166db8cf1ca61a02f3fb8c3d938c1e55ca44c77
BonLeung <654592397@qq.com>
2017-09-28 18:09:16 +0800

    commit

--- src/test/java/com/imooc/LoggerTest.java
+++ src/test/java/com/imooc/LoggerTest.java
@@ -2,6 +2,8 @@ package com.imooc;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,6 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LoggerTest {
+    private final Logger log = LoggerFactory.getLogger(LoggerTest.class);
 
     @Test
     public void test1() {
